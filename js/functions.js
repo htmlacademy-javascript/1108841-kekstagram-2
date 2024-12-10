46a510a757b863d83992aa567c0caca648bd33c9
Ilya Staroverov <ilmenskysloven@gmail.com>
2024-12-10 10:31:02 +0300

    feat: Сделать задание 7.15. Отрисуй меня полностью
    Сделано задание 7.15. Отрисуй меня полностью

--- js/functions.js
+++
@@ -1,8 +0,0 @@
-const isMeeting = (workStart, workEnd, meetingStart, meetingDuration) => {
-  const toMinutes = (time) => time.split(':').reduce((h, m) => h * 60 + +m);
-  return (
-    toMinutes(meetingStart) >= toMinutes(workStart) &&
-    toMinutes(meetingStart) + meetingDuration <= toMinutes(workEnd)
-  );
-};
-isMeeting();
--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
 import { generatePhotos } from './util.js';
+import { renderPictures } from './render-thumbnails.js';
+import { MOCK_PHOTO } from './mocks-photo.js';
+
 generatePhotos();
+renderPictures(MOCK_PHOTO);
