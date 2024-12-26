945a0287c05e088288ab693e8b6ea13f30011496
Ilya Staroverov <ilmenskysloven@gmail.com>
2024-12-26 09:24:53 +0300

    Исправил №1

--- js/show-error.js
+++ js/show-error.js
@@ -1,13 +1,15 @@
 import { ErrorSettings } from './constants.js';
 
 const showError = (message) => {
-  const errorTemplate = document.querySelector('#data-error').content;
+  const errorTemplate = document.querySelector('#data-error').content.querySelector('.data-error');
   const errorElement = errorTemplate.cloneNode(true);
   errorElement.querySelector('.data-error__title').textContent = message;
   document.body.append(errorElement);
 
   setTimeout(() => {
-    errorElement.remove();
+    if (errorElement && document.body.contains(errorElement)) {
+      errorElement.remove();
+    }
   }, ErrorSettings.SHOW_DELAY);
 };
 
